e82fd13cc2cb2bb5e101bbf125b8b95e1c20f505
Gaurav Gupta <guptgaurav@gmail.com>
2013-09-25 13:56:20 -0400

    more server side and client side logging

--- demo-3/app.js
+++ demo-3/app.js
@@ -27,8 +27,9 @@
   io.sockets.on('connection', function (socket) {
     socket.on('map_event', function (data) {
       console.log('map_event received' + data);
+      var latlng = data.split(',')
       //for the purposes of data collection and building paths for demo
-      console.log(newDate.getTime(), data.args[0], data.args[1]);
+      console.log(newDate.getTime(), latlng[0], latlng[1]);
       io.sockets.emit('map_event', data);
     });
   });
